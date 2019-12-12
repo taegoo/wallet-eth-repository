c06dbc49ae9d4b0c3a0ee48b1120c529de3daf3b
bizarim <bizarim82@gmail.com>
2019-12-12 15:51:36 +0900

    dist build

---
+++ dist/entity/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./Address"));
+__export(require("./Balance"));
+__export(require("./History"));
+__export(require("./Transaction"));
+__export(require("./Wallet"));
+//# sourceMappingURL=index.js.map
